ceaec0fc0560bd3aa6f9f853fe91f96223cf95d3
Ariiel08 <ariel_ortega98@hotmail.com>
2023-06-01 16:56:48 -0400

    Environment fix

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,30 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
-import { getEnvironments } from "../helpers/getEnvironments";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 
-const {
-    VITE_APIKEY,
-    VITE_AUTHDOMAIN,
-    VITE_PROJECTID,
-    VITE_STORAGEBUCKET,
-    VITE_MESSAGINGSENDERID,
-    VITE_APPID
-  } = getEnvironments();
 
-const firebaseConfig = {
-    apiKey: VITE_APIKEY,
-    authDomain:   VITE_AUTHDOMAIN,
-    projectId:   VITE_PROJECTID,
-    storageBucket:   VITE_STORAGEBUCKET,
-    messagingSenderId:   VITE_MESSAGINGSENDERID,
-    appId:   VITE_APPID
-};
+  const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain:   import.meta.env.VITE_AUTHDOMAIN,
+    projectId:   import.meta.env.VITE_PROJECTID,
+    storageBucket:   import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId:   import.meta.env.VITE_MESSAGINGSENDERID,
+    appId:   import.meta.env.VITE_APPID
+  };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
